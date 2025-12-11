6b1354c4dc9a018ead0c497e94dc30fde8c3a8f2
Sally <causeysally@gmail.com>
2025-12-11 15:52:43 -0600

    running express server

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ import express from "express";
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.json({ message: "weather microservice is running!!" });
+});
 
-app.get("/weather", (req, res) => {
+app.get("/weather/zip/date", (req, res) => {
   const { zip, date } = req.query;
-  if (!zip) {
-    return res.status(400).json({ error: "zip is required" });
-  }
-  if (!date) {
-    return res.status(400).json({ error: "date is required" });
-  }
+  //   if (!zip) {
+  //     return res.status(400).json({ error: "zip is required" });
+  //   }
+  //   if (!date) {
+  //     return res.status(400).json({ error: "date is required" });
+  //   }
   const weather = {
-    tempHigh: "70",
-    tempLow: "52",
-    conditins: "slightly sunny",
+    tempHigh: 70,
+    tempLow: 52,
+    conditions: "slightly sunny",
     summary: "Cool with few clouds",
   };
   res.json(weather);
